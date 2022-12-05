9468f5072565a198be78655630c30bc069ab3654
Kevin-Buck <kbuck2@nd.edu>
2022-12-05 11:18:34 -0500

    extra file deleted, WriteCSV working

--- analysis.R
+++ analysis.R
@@ -5,6 +5,7 @@
 setwd("/Users/kevinbuck/Desktop/Biocomputing/Exercises/Rproject")
 source("SupportingFunctions.R")
 
+#turning all the txt files for Country Y into CSV files
 for (i in 120:175){
   makeCSV(filename = paste("countryY/screen_",i,".txt",sep=""),delimeter = " ",
           fileOutname = paste("countryY/screen_",i,sep=""))
