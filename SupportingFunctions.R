fd6e5b85c90efa2d34a95c677b7e1f5329bfed0b
Kevin-Buck <kbuck2@nd.edu>
2022-12-07 11:23:11 -0500

    working on combineCSV functiion, not done yet :(

--- SupportingFunctions.R
+++ SupportingFunctions.R
@@ -3,3 +3,12 @@ makeCSV <- function(filename,delimeter,fileOutname){
   tempTable <- read.table(file=filename,sep=delimeter)
   write.csv(tempTable,file=paste(fileOutname,".csv",sep=""))
 }
+combineCSVs <- function(filename,outputObj,country,day){
+  tempDF <- read.csv(file=filename,header=T)
+  dayVect <- rep(day,nrow(tempDF))
+  countryVect <- rep(country,nrow(tempDF))
+  finaltempDF <- cbind(tempDF,countryVect,dayVect)
+  #print(finaltempDF)
+  outputObj <- rbind(outputObj,finaltempDF)
+  return(outputObj)
+}
--- analysis.R
+++ analysis.R
@@ -10,3 +10,16 @@ for (i in 120:175){
   makeCSV(filename = paste("countryY/screen_",i,".txt",sep=""),delimeter = " ",
           fileOutname = paste("countryY/screen_",i,sep=""))
 }
+colsList <- list("gender","age","marker01","marker02","marker03","marker04",
+                 "marker05","marker06","marker07","marker08","marker09","marker10",
+                 "country","dayofYear")
+bigDF <- matrix(data=NA,nrow=0,ncol=14)
+colnames(bigDF) = colsList
+for(i in 120:175){
+  bigDF <- combineCSVs(filename=paste("countryX/screen_",i,".csv",sep=""),outputObj=bigDF,
+          country="x",day=i)
+  bigDF <- combineCSVs(filename=paste("countryY/screen_",i,".csv",sep=""),outputObj=bigDF,
+          country="y",day=i)
+}
+
+
