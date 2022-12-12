57ae3ba472969c9dc4dfe42695de4e6fc0abab4b
avivalund <avivalund@gmail.com>
2022-12-11 20:47:56 -0500

    update with NA and summary

--- RProject2022_Submission.R
+++ RProject2022_Submission.R
@@ -65,15 +65,76 @@ for (i in 2:length(csvlistY)){
   inputY<-rbind(inputY, Yinput)
 }  
 
+# (check this) Allowing user to remove or include NA columns, with or without warnings
+if (naOption == "remove") {
+  # Remove rows with NA's in any columns
+  data <- data[complete.cases(data),]
+} else if (naOption == "warn") {
+  # Check for NA's in the data and warn the user if they are present
+  if (any(is.na(data))) {
+    warning("Data contains NA values")
+  }
+} else if (naOption == "include") {
+  # Do nothing - include NA's in the data without warning the user
+} else {
+  # Invalid option - raise an error
+  stop("Invalid value for naOption parameter")
+}
+
+# Return the compiled data frame
+return(data)
+
+# how user uses function
+# compiledData <- compileData("path/to/directory", "remove")
+  # for second argument, include "remove," "warn," or "include"
+    # "remove" for removing rows with NA's
+    # "warn" for including NA's but showing a warning message
+    # "include" for including NA's without a warning
+
 ### Combine x and y csv lists
 allData.csv<-rbind(inputY, inputX)
 setwd("/Users/avivalund/Desktop/Biocomputing/FinalProject/RProject/")
 write.csv(allData.csv, "allData.csv", row.names = F)
 
-
-
 #Script 2: analyzing script, source("supportingFunctions.R")
 
+  # Write a function to summarize the compiled data set in terms of
+  # number of screens run, percent of patients screened that were infected, 
+  # male vs. female patients, and the age distribution of patients.
+
+import allData.csv
+
+def summarize_data(allData.csv)
+  num_screens = 0
+num_infected = 0
+num_male = 0
+num_female = 0
+age_distribution = {}
+
+with open(allData.csv, 'r') as csvfile:
+  reader = csv.reader(allData.csv)
+# Skip the header row
+next(reader)
+for row in reader:
+  num_screens += 1
+if row[1] == 'Yes':
+  num_infected += 1
+if row[2] == 'Male':
+  num_male += 1
+elif row[2] == 'Female':
+  num_female += 1
+
+# Count the number of patients in each age group
+age = int(row[3])
+if age not in age_distribution:
+  age_distribution[age] = 0
+age_distribution[age] += 1
+
+print('Number of screens run:', num_screens)
+print('Percent of patients screened that were infected:', 100 * num_infected / num_screens)
+print('Number of male patients:', num_male)
+print('Number of female patients:', num_female)
+print('Age distribution of patients:', age_distribution)
 
 
 
