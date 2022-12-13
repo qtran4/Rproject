346253ee1d802ab281395cd816cfc303043ba507
nfernan4 <nfernan4@nd.edu>
2022-12-13 13:37:05 -0500

    adding analyisR script

---
+++ analysis.R
@@ -0,0 +1,13 @@
+#analysis.R script
+
+#create source path to supportingFunctions.R script
+source("/Users/avivalund/Desktop/Biocomputing/FinalProject/RProject/supportingFunctions.R")
+
+#Load functions in supportingFunctions.R file
+source("supportingFunctions.R")
+
+#Compile all data into single CSV by calling function
+compiledData()
+
+
+#Process data 
