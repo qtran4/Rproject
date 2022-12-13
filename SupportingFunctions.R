3434170e29b067431bcd2d5144ac8f5f54eb541a
Kevin-Buck <kbuck2@nd.edu>
2022-12-13 10:05:25 -0500

    Added check_infected function, plotted cases over time

--- SupportingFunctions.R
+++ SupportingFunctions.R
@@ -3,7 +3,10 @@ makeCSV <- function(filename,delimeter,fileOutname){
   tempTable <- read.table(file=filename,sep=delimeter)
   write.csv(tempTable,file=paste(fileOutname,".csv",sep=""),row.names = F)
 }
-combineCSVs <- function(directoryname,country,na_handling="none"){
+
+#This function combines all the data from a directory into a big csv file and also
+# returns this aggregated data set as a data frame
+combineCSVs <- function(directoryname,country,na_handling="none",FileOutName){
   #making list of all .csv files in directory using grep to search for .csv
   csv.files.list <- dir(directoryname)[grep(".csv",dir(directoryname))] 
   #initiating matrix for final results
@@ -33,7 +36,39 @@ combineCSVs <- function(directoryname,country,na_handling="none"){
       print("No NA Values Present")
     }
     }
+  write.csv(bigMatrix,file = paste(as.character(FileOutName),".csv",sep=""))
   return(bigMatrix)
+
 }
-  
+# This function summarizes the Data Set for a variety of aspects
+SummarizeCompiledTests <- function(dfName){
+  screen_number <- nrow(dfName)
+  running_infected_total <- 0
+  #markers.list <- c("marker01","marker02","marker03","marker04","marker05",
+  #                  "marker06","marker07","marker08","marker09","marker10")
+  for (i in 1:screen_number){
+    if(sum(as.numeric(dfName[i,3:12] == "1"))){
+      running_infected_total <- running_infected_total +1
+      }
+  }
+  percentage.male <- sum(dfName[,"gender"] == "male") / screen_number * 100
+  percentage.female <- 100 - percentage.male
+  percentage.juvenile <- sum(dfName[,"age"] < 18) / screen_number * 100
+  percentage.adult <- (sum(dfName[,"age"] < 65) / screen_number *100 ) - percentage.juvenile
+  percentage.senior <- sum( dfName[,"age"] >= 65) / screen_number *100
+  infected <- running_infected_total / screen_number *100
+  print(c("Data Summary:",
+          paste("There were ",screen_number," screening tests run",sep=""),
+          #percentage of males out of all screened
+          paste(round(percentage.male,1),"% of patients were male",sep=""), 
+          #percentage of females out of all screened
+          paste(round(percentage.female,1),"% of patients were female",sep=""),
+          #percentage of juveniles out of all screened
+          paste(round(percentage.juvenile,1),"% of patients were juveniles (under 18)",sep=""),  
+          #percentage of adults out of all screened
+          paste(round(percentage.adult,1),"% of patients were adults (18-65)",sep=""),
+          #percentage of senior citizens out of all screened
+          paste(round(percentage.senior,1),"% of patients were seniors (over 65)",sep=""))
+          )
+}
   
