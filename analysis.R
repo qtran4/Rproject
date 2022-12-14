576bd1a13fadec67453bcb6228338330845a1716
Isaiah Murrell-Thomas <imurrell@nd.edu>
2022-12-13 20:09:58 -0500

    Final Code 2

--- analysis.R
+++ analysis.R
@@ -1,22 +1,41 @@
+##R Final Project 
+# Written by Isaiah Murrell-Thomas and Bethany Oceguera
+
+## IMPORTANT INFORMATION
+# Before running this code, make sure that you are in the directory containing all relevant files.
+# You will also need to define the following variables: Dir1 and Dir2 if you are not in the working directory for this code
+# If the file allData.csv already exists then you will want to remove it every time you run this code for function 2 to work properly
+
+#Dir1 should contain your .txt files
+#Dir2 should contain your "allData.csv", "supportingFunctions.R", and "analysis.R" files 
+
+#For example:
+Dir1 = "/Users/bethoceguera/Documents/PhD Year 1/First Semester/Introduction to Biocomputing/Rproject/countryY"
+Dir2 = "/Users/bethoceguera/Documents/PhD Year 1/First Semester/Introduction to Biocomputing/Rproject"
+
 ##Below, the five functions created for this project are run via "source()"
 source("supportingFunctions.R")
 
+##Below, the five functions created for this project are run via "source()"
 ##FUNCTION 1
 # Function that converts all files in a directory with space- or 
 #   tab-delimited data (.txt) into comma-separated value files.
-dir <- 
-  "~/Documents/PhD Year 1/First Semester/Introduction to Biocomputing/Rproject/countryY" 
+dir = Dir1
 csvConvert(dir)
 
+#Explanation
+# Only country Y needs to have its files converted because country X files are already in the correct format.
+# However, if that were to change, all you you need to do is to specify the correct directory containing the files
+
 ##FUNCTION 2
-# Function that compiles data from all .csv files in a directory 
+# Function that compiles git data from all .csv files in a directory 
 # into a single .csv file. The compiled data has the original twelve columns 
 # from daily data sheets, but also country and dayofYear columns. 
 # The user should be able to choose whether they want to remove rows with NA’s 
 # in any columns, include NAs in the compiled data but be warned of their presence, or include NAs in the compiled data without a warning
-dir <- 
-  "~/Documents/PhD Year 1/First Semester/Introduction to Biocomputing/Rproject"
-naChoice <- 1
+
+dir = Dir2
+naChoice = 1
 csvMerge(dir,naChoice)
 
 ##FUNCTION 3
@@ -25,9 +44,8 @@ csvMerge(dir,naChoice)
 #   3b. Male vs. female patients
 #   3c. Age distribution of patients
 #   3d. Percent of patients screened that were infected
-dir <- 
-  "~/Documents/PhD Year 1/First Semester/Introduction to Biocomputing/Rproject"
-filename <-  "allData.csv"
+dir = Dir2
+filename = "allData.csv"
 csvAnalysis(dir,filename) #This will return a summary table of 3a - 3d
 
 ## FUNCTION 4 (Question 1)
@@ -40,7 +58,7 @@ csvAnalysis(dir,filename) #This will return a summary table of 3a - 3d
 # filename = "allDataTest.csv"
 # Run csvAnalysis2(dir,filename)
 
-dir = ":C/User/ikemu/Downloads/Rproject"
+dir = Dir2
 filename = "allData.csv"
 csvAnalysis2(dir,filename)
 
@@ -52,12 +70,11 @@ csvAnalysis2(dir,filename)
 ## FUNCTION 5 (Question 2)
 # This function compiles two bar graphs for two countries to show variation 
 # in Markers 1 - 10
-dir <- 
-  "~/Documents/PhD Year 1/First Semester/Introduction to Biocomputing/Rproject"
-file <- "allData.csv"
+dir = Dir2
+file = "allData.csv"
 markerVariation("allData.csv") #This will return the graph for both countries
 
-# Explanation
+#Explanation
 #   If Country Y develops a vaccine for the disease, it is likely it would not
 # work for country X. From the graph- SummaryPlot- one can see that those in 
 # country X test positive for mostly markers 1 - 5. On the other hand, country
