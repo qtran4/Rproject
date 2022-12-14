f24ec009fc8e061e9a7dbcd11518354f03e4005d
Isaiah Murrell-Thomas <imurrell@nd.edu>
2022-12-13 20:09:14 -0500

    Final Code

--- supportingFunctions.R
+++ supportingFunctions.R
@@ -1,5 +1,8 @@
 ##R Final Project
-setwd("/Users/bethoceguera/Documents/PhD Year 1/First Semester/Introduction to Biocomputing/Rproject")
+# Written by Isaiah Murrell-Thomas and Bethany Oceguera 
+
+## IMPORTANT INFORMATION
+# Before running this code, please make sure that you are in the directory containing all relevant files 
 
 ##FUNCTION 1
 # Function that converts all files in a directory with space- or 
@@ -9,11 +12,11 @@ csvConvert <- function(dir){
   setwd(dir)
   filesToConvert <- list.files(pattern = ".txt") 
   for (i in 1:length(filesToConvert)){
-  input <- filesToConvert[i]  
-  output <- paste0(gsub("\\.txt$", "", input), ".csv")
-  data <- read.table(input, sep="",header=TRUE)
-  write.table(data, file=output, sep=",", col.names=TRUE, row.names=FALSE)
-  file.remove(input)  
+    input <- filesToConvert[i]  
+    output <- paste0(gsub("\\.txt$", "", input), ".csv")
+    data <- read.table(input, sep="",header=TRUE)
+    write.table(data, file=output, sep=",", col.names=TRUE, row.names=FALSE)
+    file.remove(input)  
   }
   print("Files have been converted to .csv")
 }
@@ -67,6 +70,7 @@ csvMerge<-function(dir,naChoice){
   print("Country X and Y files have been combined to a master file called allDataTest.csv")
 }
 
+#For the rest of the funcitons, we used "allData.csv" 
 #USAGE 
 # Define and run variable "dir" as the directory path that holds the files to be converted
 # Define and run variable "naChoice":
@@ -88,7 +92,7 @@ csvMerge<-function(dir,naChoice){
 #   3c. Age distribution of patients
 #   3d. Percent of patients screened that were infected
 
-csvAnalysis = function(dir,filename){
+csvAnalysis <-  function(dir,filename){
   ## set working directory
   setwd(dir)
   
@@ -115,7 +119,6 @@ csvAnalysis = function(dir,filename){
   Answer_3a = "Any age over 100 is not represented in this graph, but they make up 178 data points." 
   Answer_3b = "However, based on this graph, it appears that the infection is effecting mostly people between the ages of 0-10"
   Answer_3 = paste(Answer_3a, Answer_3b)
-  
   ## Percent of people infected
   infectedcounter = 0
   for(i in 1:nrow(AllData)){
@@ -131,21 +134,52 @@ csvAnalysis = function(dir,filename){
   Answer_4b = "percent"
   Answer_4 = paste(Answer_4a, percentinfected, Answer_4b)
   Answer_list = list(Answer_1, Answer_2, Answer_3, Answer_4)
+  #Summary Table
+  summaryTable <- data.frame(Parameter=c("Total Screen Runs",
+                                         "Percent Positive","Male Count",
+                                         "Female Count","Age 0-10","Age 11-20", 
+                                         "Age 21-30","Age 31-40","Age 41-50",
+                                         "Age 51-60","Age 61-70","Age 71-80",
+                                         "Age 80+"),
+                             Value=c(screenstotal,percentinfected,malecount,
+                                     femalecount,sum(AllData$age%in%(0:10)),
+                                     sum(AllData$age%in%(11:20)),
+                                     sum(AllData$age%in%(21:30)),
+                                     sum(AllData$age%in%(31:40)),
+                                     sum(AllData$age%in%(41:50)),
+                                     sum(AllData$age%in%(51:60)),
+                                     sum(AllData$age%in%(61:70)),
+                                     sum(AllData$age%in%(71:80)),
+                                     nrow(AllData[AllData$age>=81,])))
+  return(summaryTable)
   return(Answer_list)
 }
 
 #USAGE 
-# Define and run variable "dir" as the directory path that holds the file to run analysis on
-# Define and run variable "filename" as the file within dir to have analysis ran on
-# EXAMPLE: Run an analysis on the compiled data after having run the csvMerge function
-# dir = ":C/User/ikemu/Downloads/Rproject"
-# filename = "allDataTest.csv"
+# Define and run variable "dir" as the directory path that holds the files to be converted
+# Define and run variable "filename" as the file within dir to be 
+# EXAMPLE: Run an analysis on the compiled data after having run the CSVMerge function
+# dir <-  ":C/User/ikemu/Downloads/Rproject"
+# filename <-  "allDataTest.csv"
 # Run "csvAnalysis(dir,filename)"
+
+
+#USAGE 
+# Define and run variable "dir" as the directory path that holds the files to be converted
+# Define and run variable "filename" as the file within dir to be 
+# EXAMPLE: Run an analysis on the compiled data after having run the CSVMerge function
+# dir <-  ":C/User/ikemu/Downloads/Rproject"
+# filename <-  "allDataTest.csv"
+# Run "csvAnalysis(dir,filename)"
+
 
 ##FUNCTION 4
 # Function to create a time graph of infection for country X and country Y
 
 csvAnalysis2 = function(dir, filename){
+  # Set working directory
+  setwd(dir)
+  
   # Load Data and subset into country specific data
   AllData = read.csv(filename)
   file_x = subset(AllData, country == "X")
@@ -181,7 +215,7 @@ csvAnalysis2 = function(dir, filename){
     x_vec[i] = nrow(infectedXfile[infectedXfile$dayofYear == dayX,])
     dayX = dayX + 1
   }
-
+  
   ## Country Y for loop
   dayY = 120
   for(i in 1:length(y_vec)){
@@ -197,7 +231,7 @@ csvAnalysis2 = function(dir, filename){
   library(ggplot2)
   
   ## Plot
-  ggplot(combined_data, aes(time_vec)) + geom_line(aes(y=x_vec), color = "black") + geom_line(aes(y=y_vec), color = "blue") + xlab("Day of Year") +ylab("Number of Infections") + xlim(120,181) + ggtitle("Infections Per Day for Country X and Country Y")
+  ggplot(combined_data, aes(time_vec)) + geom_line(aes(y=x_vec), color = "black") + geom_line(aes(y=y_vec), color = "blue") + xlab("Day of Year") +ylab("Number of Infections") + xlim(120,181) + ggtitle("Infections Per Day for Country X (Black) and Country Y (Blue)")
   print("The black line is country x infections per day and the blue line is country y infections per day")
 }
 
@@ -211,10 +245,60 @@ csvAnalysis2 = function(dir, filename){
 # Run "csvAnalysis2(dir,filename)"
 
 
-  
- 
-  
-  
-  
 
+## FUNCTION 5
+# If Country Y develops a vaccine for the disease, is it likely to work for 
+# citizens of Country X?
+# This function compiles two bar graphs for country X and Y to show variation 
+# in Markers 1 - 10
+
+markerVariation <- function(file){
+  
+  setwd(dir)
+  
+  library(ggplot2)
+  library(cowplot) 
+  
+  data <- read.csv(file)
+  dfX <- data[data$country == "X",]
+  dfY <- data[data$country == "Y",]
+  df1 <- data.frame(Marker=c("1","2","3","4","5","6","7","8","9","10"),
+                    XFrequency=c(nrow(dfX[dfX$marker01==1,]),
+                                 nrow(dfX[dfX$marker02==1,]),
+                                 nrow(dfX[dfX$marker03==1,]),
+                                 nrow(dfX[dfX$marker04==1,]),
+                                 nrow(dfX[dfX$marker05==1,]),
+                                 nrow(dfX[dfX$marker06==1,]),
+                                 nrow(dfX[dfX$marker07==1,]),
+                                 nrow(dfX[dfX$marker08==1,]),
+                                 nrow(dfX[dfX$marker09==1,]),
+                                 nrow(dfX[dfX$marker10==1,])),
+                    YFrequency=c(nrow(dfY[dfY$marker01==1,]),
+                                 nrow(dfY[dfY$marker02==1,]),
+                                 nrow(dfY[dfY$marker03==1,]),
+                                 nrow(dfY[dfY$marker04==1,]),
+                                 nrow(dfY[dfY$marker05==1,]),
+                                 nrow(dfY[dfY$marker06==1,]),
+                                 nrow(dfY[dfY$marker07==1,]),
+                                 nrow(dfY[dfY$marker08==1,]),
+                                 nrow(dfY[dfY$marker09==1,]),
+                                 nrow(dfY[dfY$marker10==1,])))
+  
+  XFrequency <- 
+    ggplot(df1, aes(x=Marker, y=XFrequency)) + geom_bar(stat = "identity")
+  YFrequency <- 
+    ggplot(df1, aes(x=Marker, y=YFrequency)) + geom_bar(stat = "identity")
+  
+  SummaryPlot <- 
+    plot_grid(XFrequency,YFrequency,labels=c("a","b"),rel_widths=c(1,1),ncol=2,nrow=1)
+  return(SummaryPlot)
+}
+
+#USAGE 
+# Define and run variable "dir" as the directory path that holds the files to be converted
+# Define and run variable "file" as the file that holds all data for both countries (i.e. allData.csv)
+# EXAMPLE: Compile two barcharts that show the Marker distribution for countries X and Y
+# dir <- "/Users/bethoceguera/Documents/PhD Year 1/First Semester/Introduction to Biocomputing/Rproject"
+# file <- "allData.csv"
+# Run "markerVariation("allData.csv")"
 
