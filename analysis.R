eb40e18199a31b5ac78916c81e0675c1597b3422
haydengallo <hgallo@nd.edu>
2022-12-13 21:57:22 -0500

    Next iteration. Believe it is done.

--- analysis.R
+++ analysis.R
@@ -5,10 +5,11 @@ library(ggplot2)
 
 source('./supportingFunctions.R')
 
+summarize_compiled('allData.csv', 'no') # use summarize function to show summary stats for allData.csv file
 
 #1. In which country (X or Y) did the disease outbreak likely begin?
 
-delim_convert('countryY')
+delim_convert('countryY') #choose to convert all of the .txt files in countryY directory to .csvs
 
 # compile all country data
 # 3 choices for NA reporting with compile_csv
@@ -16,28 +17,37 @@ delim_convert('countryY')
 # 2) 'warn', warns about NA values, but does not remove them
 # 3)  supplying no argument does nothing
 
-compile_csv('countryY', 'remove')
-compile_csv('countryX', 'remove')
+countryY_data <- compile_csv('countryY', 'remove') #compile all files in countryY directory while removing NAs if present
+countryX_data <- compile_csv('countryX', 'remove') #compile all files in countryX directory while removing NAs if present
 
-countryY_summary <- summarize_compiled('countryY')
-countryX_summary <- summarize_compiled('countryX')
+all_data_compiled <- rbind(countryX_data, countryY_data) #bind both countrie data
+write.csv(all_data_compiled, paste0('./all_data_compiled.csv'), row.names = FALSE) #save all data to .csv
 
-countryX_data <- read.csv('./countryX/compiled_data_countryX.csv', header = TRUE)
-countryY_data <- read.csv('./countryY/compiled_data_countryY.csv', header = TRUE)
+countryY_summary <- summarize_compiled('countryY', 'yes') #Summary of countryY
+countryX_summary <- summarize_compiled('countryX', 'yes') #Summary of countryX
 
+countryX_data['sum'] <- rowSums(countryX_data[,c(3:12)]) #sum marker columns
+countryY_data['sum'] <- rowSums(countryY_data[,c(3:12)]) #sum marker columns
+all_data_compiled['sum'] <- rowSums(all_data_compiled[,c(3:12)]) #sum marker columns
 
-countryX_data['sum'] <- rowSums(countryX_data[,c(3:12)])
-countryY_data['sum'] <- rowSums(countryY_data[,c(3:12)])
+countryX_data['infected'] <- ifelse(countryX_data['sum'] >= 1,1,0) #if sum row is greater or equal to 1 then person infected and labeled as 1 in infecetion column
+countryY_data['infected'] <- ifelse(countryY_data['sum'] >= 1,1,0) #if sum row is greater or equal to 1 then person infected and labeled as 1 in infecetion column
+all_data_compiled['infected'] <- ifelse(all_data_compiled['sum'] >= 1,1,0) #if sum row is greater or equal to 1 then person infected and labeled as 1 in infecetion column
 
-countryX_data['infected'] <- ifelse(countryX_data['sum'] >= 1,1,0)
-countryY_data['infected'] <- ifelse(countryY_data['sum'] >= 1,1,0)
+countryX_infections_day <- aggregate(countryX_data$infected~countryX_data$dayofYear, countryX_data, sum) #create aggregated infections table by country and day of year
+countryY_infections_day <- aggregate(countryY_data$infected~countryY_data$dayofYear, countryY_data, sum) #create aggregated infections table by country and day of year
+all_data_infections_day <- aggregate(all_data_compiled$infected, list(all_data_compiled$dayofYear, all_data_compiled$country), FUN=sum) #create aggregated infections table by country and day of year
+colnames(all_data_infections_day) <- c('Day', 'Country', 'Sum')
 
-countryX_infections_day <- aggregate(countryX_data$infected~countryX_data$dayofYear, countryX_data, sum)
-countryY_infections_day <- aggregate(countryY_data$infected~countryY_data$dayofYear, countryY_data, sum)
-
-ggplot() + 
+ggplot() + #plot line plot of daily infections by country across the days of the year given
   geom_line(data = countryX_infections_day, aes(x = `countryX_data$dayofYear`, y = sum, color = 'Country X')) + 
   geom_line(data = countryY_infections_day, aes(x = `countryY_data$dayofYear`, y = sum, color = 'Country Y')) +
+  xlab('Day of Year') + 
+  ylab('Number of Infections Per Day') + 
+  ggtitle('Infections Per Day by Country')
+
+ggplot() + #plot line plot of daily infections by country across the days of the year given, same plot as before just using total compiled data instead of both countries' data
+  geom_line(data = all_data_infections_day, aes(x = Day, y = Sum, color = Country)) + 
   xlab('Day of Year') + 
   ylab('Number of Infections Per Day') + 
   ggtitle('Infections Per Day by Country')
@@ -52,21 +62,19 @@ ggplot() +
 
 # plot distributions of markers 
 
-marker_sum_country_X <- data.frame(colSums(countryX_data[,c(3:12)]))
-marker_sum_country_Y <- data.frame(colSums(countryY_data[,c(3:12)]))
+marker_sum_country_X <- data.frame(colSums(countryX_data[,c(3:12)])) #sum down marker columns
+marker_sum_country_Y <- data.frame(colSums(countryY_data[,c(3:12)])) #sum down marker columns
 
+marker_sum_country_X <- cbind(rownames(marker_sum_country_X), data.frame(marker_sum_country_X, row.names=NULL)) #make marker names row names
+marker_sum_country_Y <- cbind(rownames(marker_sum_country_Y), data.frame(marker_sum_country_Y, row.names=NULL)) #make marker names row names
 
+colnames(marker_sum_country_X) <- c('marker','instances') #change column names
+colnames(marker_sum_country_Y) <- c('marker','instances') #change column names
 
-marker_sum_country_X <- cbind(rownames(marker_sum_country_X), data.frame(marker_sum_country_X, row.names=NULL))
-marker_sum_country_Y <- cbind(rownames(marker_sum_country_Y), data.frame(marker_sum_country_Y, row.names=NULL))
+marker_sum_country_X$instances <- marker_sum_country_X$instances/sum(marker_sum_country_X$instances) #normalize instances of markers to 1
+marker_sum_country_Y$instances <- marker_sum_country_Y$instances/sum(marker_sum_country_Y$instances) #normalize instances of markers to 1
 
-colnames(marker_sum_country_X) <- c('marker','instances')
-colnames(marker_sum_country_Y) <- c('marker','instances')
-
-marker_sum_country_X$instances <- marker_sum_country_X$instances/sum(marker_sum_country_X$instances)
-marker_sum_country_Y$instances <- marker_sum_country_Y$instances/sum(marker_sum_country_Y$instances)
-
-ggplot() + 
+ggplot() + #graph barplot of different marker prevalence to determine different distribution of markers in populations
   geom_bar(marker_sum_country_X, stat = 'identity', position = 'dodge', mapping = aes(x=marker, y=instances, fill = 'Country X'), alpha = 0.8) +
   geom_bar(marker_sum_country_Y, stat = 'identity', position = 'dodge', mapping = aes(x=marker, y=instances, fill = 'Country Y'), alpha = 0.8) +
   xlab('Marker') + 
@@ -81,5 +89,4 @@ ggplot() +
 # markers 1-5. So based on the countries having different distributions of the prevalence of markers,
 # it would seem to be that a vaccine developed for Country Y would not be very effective for the citizens
 # of Country X.
-
 
--- supportingFunctions.R
+++ supportingFunctions.R
@@ -4,57 +4,59 @@
 
 delim_convert <- function(directory_name){
 
-  filenames <- list.files(path = paste0('./',directory_name), pattern = "\\.txt$") 
+  filenames <- list.files(path = paste0('./',directory_name), pattern = "\\.txt$") #grab all file names with .txt
   count = 120
   for (i in filenames){
-    sample <- read.table(paste0('./',directory_name,'/',i), header = TRUE)
-    write.csv(sample, paste0('./',directory_name,'/screen_',count,'.csv'), row.names = FALSE)
-    count = count + 1 
+    sample <- read.table(paste0('./',directory_name,'/',i), header = TRUE) #loop through each of the .txt files grabbing one at a time
+    write.csv(sample, paste0('./',directory_name,'/screen_',count,'.csv'), row.names = FALSE) #writing each .txt file to .csv
+    count = count + 1 #iterating count for file naming scheme
   }
 
 }
 
 compile_csv <- function(directory_name, NA_preference){
-  filenames <- list.files(path = paste0('./',directory_name), pattern = "\\.csv$")
+  filenames <- list.files(path = paste0('./',directory_name), pattern = "\\.csv$") #grab all file names with .csv
   count = 120
-  for (i in filenames){
-    if (!exists('compiled_data')){
-      compiled_data <- read.csv(file = paste0('./',directory_name,'/',i), header = TRUE)
-      compiled_data['country'] <- directory_name
-      compiled_data['dayofYear'] <- count
+  for (i in filenames){ #iteratre over all .csvs in directory
+    if (!exists('compiled_data')){ #if original compile file isn't created create it
+      compiled_data <- read.csv(file = paste0('./',directory_name,'/',i), header = TRUE) #load first file
+      compiled_data['country'] <- directory_name #add country name column
+      compiled_data['dayofYear'] <- count #add day of year column
     }
     
-    if (exists('compiled_data')){
+    if (exists('compiled_data')){ #go here if compile data file is already created
       
-      temp <- read.csv(paste0('./',directory_name,'/',i))
-      temp['country'] <- directory_name
-      temp['dayofYear'] <- count
-      compiled_data <- rbind(compiled_data, temp)
+      temp <- read.csv(paste0('./',directory_name,'/',i)) #read each of files one by one
+      temp['country'] <- directory_name # add country name column
+      temp['dayofYear'] <- count # add day of year column
+      compiled_data <- rbind(compiled_data, temp) #merge temp file with compile data file
     }
-    count = count + 1
+    count = count + 1 #iterate count for name and day of year
   }
-  if(NA_preference == 'remove'){
+  if(NA_preference == 'remove'){ #if user wants NAs removed this loop does it
     compiled_data <- na.omit(compiled_data)
   }
-  if(NA_preference == 'warn'){
-    print('There are NA values')
-  }
-  write.csv(compiled_data, paste0('./',directory_name,'/compiled_data_',directory_name,'.csv'), row.names = FALSE)
-  
+  if(NA_preference == 'warn'){ # if users want to warn about NAs this loop does it 
+    if(anyNA(countryX_data) == 'FALSE'){
+      print('There are no NA values')} else{print("There are NA values")}
+  }# no loop for if user wants no warning of NAs
+  write.csv(compiled_data, paste0('./',directory_name,'/compiled_data_',directory_name,'.csv'), row.names = FALSE) #write final compiled dataframe to a .csv
+  return(compiled_data)
 }
 
 
-summarize_compiled <- function(country_name){
+summarize_compiled <- function(file_name, specific_country){ #include file name and if it is specific country data or all data
   
-  
-  compiled_data <- read.csv(paste0('./',country_name,'/compiled_data_',country_name,'.csv'))
-  num_screen_runs <- nrow(compiled_data)
-  age_distribution <- table(compiled_data[,2])
-  compiled_data['sum'] <- rowSums(compiled_data[,c(3:12)])
-  num_infected_percent <- (num_screen_runs - colSums(compiled_data['sum'] == 0))/num_screen_runs
-  male_v_female <- table(compiled_data[,1])
-  stuff_to_return <- list('num_screen_runs' = num_screen_runs, 'age_dist' = age_distribution, 'inf_percent' = num_infected_percent, 'm_v_f' = male_v_female)
-  return(stuff_to_return)
+  if (specific_country == 'yes'){
+    country_name <- file_name
+    compiled_data <- read.csv(paste0('./',country_name,'/compiled_data_',country_name,'.csv'))
+  }else {compiled_data <- read.csv(paste0('./',file_name))} #opens csv of all compiled data that was provided
+  num_screen_runs <- nrow(compiled_data) # number of rows of data is number of screens
+  age_distribution <- table(compiled_data[,2]) #table function shows age distribution
+  compiled_data['sum'] <- rowSums(compiled_data[,c(3:12)]) #sum across columns to see who was infections
+  num_infected_percent <- (num_screen_runs - colSums(compiled_data['sum'] == 0))/num_screen_runs #total runs minus columns containing 0 divided by runs shows total infected
+  male_v_female <- table(compiled_data[,1]) #table again for male vs female
+  stuff_to_return <- list('num_screen_runs' = num_screen_runs, 'age_dist' = age_distribution, 'inf_percent' = num_infected_percent, 'm_v_f' = male_v_female) # return the 4 different values in one list
+  return(stuff_to_return) #return list of values to return
 }
-
 
